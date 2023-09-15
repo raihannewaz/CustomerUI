ba0176595c7b67bcc88e88f2fcf1f80ee3fa55cd
raihannewaz <raihan_rn02@yahoo.com>
2023-09-15 10:35:19 +0600

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'CustomerUI';
+export class AppComponent implements OnInit {
+ title = 'GadgetPoint';
+ products: Product[] | undefined;
+
+  constructor(private http: HttpClient){}
+
+
+ngOnInit():void{
+  
+this.http.get<Pagination>('http://localhost:5250/api/Product?pageSize=3').subscribe((response: Pagination )=>{
+  console.log(response);
+  this.products = response.data;
+})
+
+}
+  
 }
---
+++ src/app/models/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    productId: number
+    productName: string
+    description: string
+    price: number
+    productImage: string
+    category: string
+    subCategory: string
+    brand: string
+    isActive: boolean
+  }
